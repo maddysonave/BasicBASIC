ac3de8605276029a8d6e71424243f93a5ac1116f
Maddy Wu <mw19@williams.edu>
2024-04-29 23:17:41 -0400

    for now

--- code/Evaluator.fs
+++ code/Evaluator.fs
@@ -3,6 +3,6 @@ open Parser
 
 let rec prettyprint (e : Expr) : string = 
     match e with 
-    | Bstring(s) -> "'" + s.ToString() + "'"
+    | Bstring(s) -> '"'+ s.ToString() + '"'
     | Print(e) -> "PRINT '" + prettyprint e + "'"
     
