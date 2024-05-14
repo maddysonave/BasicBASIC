ba5afae601db3579fed057e4d8c47693a1e1d2b1
26ahe3 <ahe3>
2024-05-14 00:10:52 -0400

    fixed test logic, should work now

--- code/tests/Tests.fs
+++ code/tests/Tests.fs
@@ -13,13 +13,10 @@ type TestClass () =
     [<TestMethod>]
       member this.TestAddition() =
         let result = parse "2 + 3"
+        printfn "%A" result
         let expected = 5
         match result with
-        | Some (Num (n)) -> Assert.AreEqual(expected, n)
+        | Some (Plus (Num(a), Num(b))) -> Assert.AreEqual(expected, (a+b))
         | _ -> Assert.Fail("Parsing failed or returned wrong type")
         
-
-        //let actual = result + expected
-
-        //Assert.AreEqual(expected, actual)
         
